634b610f932ab8a8ae0f46ac627ed3b324dc5423
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-08 20:17:45 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function App(){
+  return (
+    <Text>Ola</Text>
+  );
+};
